456702f1f5875dacf758e2f19f74973f1b5a6356
Thomas Pope <tpope0928@gmail.com>
2019-08-11 19:26:59 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205318) do
+ActiveRecord::Schema.define(version: 20190811192624) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,8 +18,12 @@ ActiveRecord::Schema.define(version: 20140212205318) do
   end
 
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
+    t.index ["passenger_id"], name: "index_rides_on_passenger_id"
+    t.index ["taxi_id"], name: "index_rides_on_taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
